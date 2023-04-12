77478aeedae9dce01a349aee84ca9ead2e490224
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-04-12 17:55:20 +0300

    Refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,47 @@ import HtmlPlugin         from "html-webpack-plugin"
 import CssMinimizerPlugin from "css-minimizer-webpack-plugin"
 import TerserPlugin       from "terser-webpack-plugin"
 
-const production = process.env.NODE_ENV === "production"
+export default env => {
+    const production = env.production ?? false
 
-export default {
-    mode:   production ? "production" : "development",
-    output: {
-        clean:    true,
-        filename: production ? "bundle.[contenthash].js" : "bundle.js",
-    },
-    devtool: production ? undefined : "inline-source-map",
-    module:  {
-        rules: [
-            {
-                test: /\.css$/i,
-                use:  [
-                    "style-loader",
-                    "css-loader",
-                ],
-            },
-            {
-                test: /\.html$/i,
-                use:  [
-                    "html-loader",
-                ],
-            },
-        ]
-    },
-    optimization: {
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin()
+    return {
+        mode:   production ? "production" : "development",
+        output: {
+            clean:    true,
+            filename: production ? "bundle.[contenthash].js" : "bundle.js",
+        },
+        devtool: production ? undefined : "inline-source-map",
+        module:  {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use:  [
+                        "style-loader",
+                        "css-loader",
+                    ],
+                },
+                {
+                    test: /\.html$/i,
+                    use:  [
+                        "html-loader",
+                    ],
+                },
+            ]
+        },
+        optimization: {
+            minimizer: [
+                new CssMinimizerPlugin(),
+                new TerserPlugin()
+            ],
+        },
+        plugins: [
+            new HtmlPlugin({
+                template: "./src/index.html",
+            }),
         ],
-    },
-    plugins: [
-        new HtmlPlugin({
-            template: "./src/index.html",
-        }),
-    ],
-    devServer: {
-        port: 8000,
-        hot:  true,
-    },
+        devServer: {
+            port: 8000,
+            hot:  true,
+        },
+    }
 }
