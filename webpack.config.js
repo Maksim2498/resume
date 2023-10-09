b62b1140be28dbf3047d813c3e76abc7b61d4a5e
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-10-09 16:52:49 +0300

    Refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,15 @@ export default env => {
     const production = env.production ?? false
 
     return {
-        mode:   production ? "production" : "development",
+        mode:    production ? "production" : "development",
+        devtool: production ? undefined : "inline-source-map",
+
         output: {
             clean:    true,
             filename: production ? "bundle.[contenthash].js" : "bundle.js",
         },
-        devtool: production ? undefined : "inline-source-map",
-        module:  {
+
+        module: {
             rules: [
                 {
                     test: /\.css$/i,
@@ -21,25 +23,32 @@ export default env => {
                         "css-loader",
                     ],
                 },
+
                 {
                     test: /\.html$/i,
-                    use:  [
-                        "html-loader",
-                    ],
+                    use: ["html-loader"],
                 },
-            ]
+
+                {
+                    test: /\.(svg|jpeg)$/i,
+                    type: "asset/resource",
+                },
+            ],
         },
+
         optimization: {
             minimizer: [
                 new CssMinimizerPlugin(),
                 new TerserPlugin()
             ],
         },
+
         plugins: [
             new HtmlPlugin({
                 template: "./src/index.html",
             }),
         ],
+
         devServer: {
             port: 8000,
             hot:  true,
